2ec6ce796ae4102f94a4ebc338cc376c10b33694
Akshay <akshaygoti23@gmail.com>
2024-04-10 20:17:08 +0530

    add-style/first-componenets

---
+++ lecture3/newapp/src/Components/Img.js
@@ -0,0 +1,9 @@
+
+
+function Img (){
+    return <> <img src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?size=626&ext=jpg&ga=GA1.1.2082370165.1710892800&semt=ais" alt="" />
+                {/* <img src="https://i.pinimg.com/originals/e8/00/1c/e8001c24a5b6925919833fa02c0fbe39.jpg" alt=" not " className="" /> */}
+            </>
+}
+
+export default Img;
---
+++ lecture3/newapp/src/Components/Pera.js
@@ -0,0 +1,41 @@
+import React from "react"
+
+// function Pera(){
+//     return <p>You can also directly specify the project name and the template you want to use via additional command line options. For example, to scaffold a Vite + Vue project, run:</p>
+// }
+
+function Pera1(){
+
+    return <p>
+        <p className="color: rgb(254 249 195);text-2xl">lorem You can also directly specify the project name and the template you want to use via additional command line options. For example, to scaffold a Vite + Vue project, run:</p>
+        </p>
+}
+function Pera2(){
+    return <p className="color: rgb(254 249 195); text-2xl">You can also directly specify the project name and the template you want to use via additional command line options. For example, to scaffold a Vite + Vue project, run:</p>
+}
+function Pera3(){
+    return <p>You can also directly specify the project name and the template you want to use via additional command line options. For example, to scaffold a Vite + Vue project, run:</p>
+}
+function Pera4(){
+    return <p>You can also directly specify the project name and the template you want to use via additional command line options. For example, to scaffold a Vite + Vue project, run:</p>
+}
+
+function Pera5(){
+    return <p>You can also directly specify the project name and the template you want to use via additional command line options. For example, to scaffold a Vite + Vue project, run:</p>
+}
+
+function Pera6(){
+    return <ul>
+        <li>menu</li>
+        <li>button</li>
+        <li>number</li>
+        <li>menu</li>
+        <li>ok</li>
+        <li>done</li>
+        <li>button</li>
+        <li>number</li>
+    </ul>
+}
+
+// export default Pera;
+export {Pera1,Pera2,Pera3,Pera4,Pera5,Pera6};
