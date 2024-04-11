29faae38899839fa96ac35229c0a2fbe0d501773
Akshay <akshaygoti23@gmail.com>
2024-04-11 11:24:39 +0530

    useEffect/table-task/

---
+++ lecture7/newapp/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Table from "./Hooks/Table";
+// import UseEffect from "./Hooks/UseEffect";
+
+function App() {
+
+  return (
+    <>
+    <h1>table example using useEffect</h1>
+    {/* <UseEffect/> */}
+    <Table/>
+    </>
+  );
+}
+
+export default App;
