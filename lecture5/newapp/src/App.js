cdd2420dc94ea17d67e1b935556933e5b7c7f5ee
Akshay <akshaygoti23@gmail.com>
2024-04-13 08:33:29 +0530

    Task

---
+++ lecture5/newapp/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import "./index.css";
+
+import Task from "./Component/Task.js";
+
+
+function App() {
+  return <>
+   <div className="App"> 
+       <Task/>
+    </div>
+  </>
+}
+
+export default App;
