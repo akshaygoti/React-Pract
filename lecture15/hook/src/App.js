e8aa37131b520c91e7196ef6465f67f282118821
Akshay <akshaygoti23@gmail.com>
2024-04-14 22:57:18 +0530

    UseEffect/tabledata

---
+++ lecture15/hook/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Table from "./Hooks/Table";
+// import UseEffect from "./Hooks/UseEffect";
+
+function App() {
+
+  return (
+    <>
+    <h1>table example using useEffect</h1>
+    {/* <UseEffect/> */}
+    <Table/>
+    </>
+  );
+}
+export default App
